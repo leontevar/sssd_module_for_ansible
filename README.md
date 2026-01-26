fef13f4852904c9d47fde8bc3ef60ec015141285
leontev alexander <leontevar@vk.com>
2026-01-26 20:45:00 +0300

    first huge commit

---
+++ README.md
@@ -0,0 +1,160 @@
+# This is an sssd module for ansible
+
+#### This module developed  with:
+### https://docs.ansible.com/ansible/latest/dev_guide/developing_modules_general.html
+
+#### Here you can read  how to code your project to make it able to run on the target host with Python2 or Python3: 
+### https://docs.ansible.com/ansible/latest/dev_guide/developing_python_3.html#developing-python-3
+
+### Usage:
+1. NECESSARY! First of all the package sssd should be installed and configured on the target server (remote host)
+2. NECESSARY! On the ansible controller one of two thing is required:
+a) "interpreter_python = python3" set in the [defaults] section of ansible.cfg
+b) your ansible role should contain a var "ansible_python_interpreter: /usr/bin/python3" set
+
+example of "a)":
+```
+[defaults]
+log_path                = ./ansible.log
+fact_caching_connection = ./out
+host_key_checking       = False
+ask_pass                = False
+callback_whitelist      = profile_tasks
+stdout_callback         = full_skip
+vault_password_file     = ~/.ssh/ansible.yml
+interpreter_python       = python3
+```
+example of "b)":
+```ansible
+- hosts: "{{ srv | default('none') }}"
+  tasks:
+    - name: add a single domain group
+      sssd:
+        domain_group: lalalagroup
+        group_state: present
+        become: yes
+        vars: 
+          ansible_python_interpreter: /usr/bin/python3
+```
+3. NECESSARY! Create dir named "library" in your project and place sssd.py there
+4. Run your play
+
+
+
+
+### Debugging:
+#### Local debugging:
+connect to server with ssh
+install package ansible
+install package `sssd` and configure /etc/sssd/sssd.conf 
+check groups already added to realm using `realm list | grep -i permitted-gr`
+clone project and step to project folder
+add new group with this module using `python3 sssd.py example/jsonfile_for_localcheck-addgrp.json`
+check groups already added to realm using `realm list | grep -i permitted-gr`
+
+#### Remote-host debugging:
+Run this module in ad-hoc mode, for example:
+```
+ANSIBLE_LIBRARY=./library ansible -i /path/to/inventory_file servername1 -m sssd -a 'domain_group=lalalagroup group_state=present' -b
+```
+
+
+
+
+#### Example /etc/sssd/sssd.conf for TESTS:
+```
+[sssd]
+domains = example.comm
+config_file_version = 2
+reconnection_retries = 3
+sbus_timeout = 30
+
+[domain/example.comm]
+ad_server = servername.example.comm
+ad_domain = example.comm
+krb5_realm = example.comm
+realmd_tags = manages-system joined-with-adcli
+cache_credentials = True
+id_provider = ad
+ad_enabled_domains = example.comm
+krb5_validate = False
+krb5_store_password_if_offline = True
+krb5_ccachedir = /var/tmp
+default_shell = /bin/bash
+ldap_id_mapping = True
+use_fully_qualified_names = True
+fallback_homedir = /home/%d/%u
+access_provider = simple
+ignore_group_members = True
+subdomain_inherit = ignore_group_members
+timeout = 300
+simple_allow_groups = testgroup1, testgroup2
+```
+
+
+
+
+
+
+
+
+### EXAMPLE of Local debugging
+```commandline
+19:03:37 fill-VirtualBox sssd_module_for_ansible$ ansible --version
+ansible [core 2.16.3]
+  config file = /app/pycharm_projects/sssd_module_for_ansible/ansible.cfg
+  configured module search path = ['/root/.ansible/plugins/modules', '/usr/share/ansible/plugins/modules']
+  ansible python module location = /usr/lib/python3/dist-packages/ansible
+  ansible collection location = /root/.ansible/collections:/usr/share/ansible/collections
+  executable location = /usr/bin/ansible
+  python version = 3.12.3 (main, Aug 14 2025, 17:47:21) [GCC 13.3.0] (/usr/bin/python3)
+  jinja version = 3.1.2
+  libyaml = True
+19:04:02 fill-VirtualBox sssd_module_for_ansible$ python3 --version
+Python 3.12.3
+19:04:12 fill-VirtualBox sssd_module_for_ansible$ uname -rv
+6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2
+19:04:14 fill-VirtualBox sssd_module_for_ansible$
+19:05:53 fill-VirtualBox sssd_module_for_ansible$ id
+uid=0(root) gid=0(root) groups=0(root)
+19:05:56 fill-VirtualBox sssd_module_for_ansible$ realm list | grep -i permitted-gr
+  permitted-groups: testgroup1, testgroup2
+19:06:17 fill-VirtualBox sssd_module_for_ansible$ cat example/jsonfile_for_localcheck-addgrp.json
+{
+    "ANSIBLE_MODULE_ARGS": {
+        "domain_group": "lalalalalalalagroup",
+        "group_state": "present"
+    }
+}
+19:06:19 fill-VirtualBox sssd_module_for_ansible$ python3 sssd.py example/jsonfile_for_localcheck-addgrp.json
+
+{"changed": true, "stdout": "Group lalalalalalalagroup now added to the server", "invocation": {"module_args": {"domain_group": "lalalalalalalagroup", "group_state": "present", "place_default_config": false, "name": null, "in_domain": null, "admin_user": null, "admin_password": null, "computer_ou": null}}, "warnings": ["Module did not set no_log for admin_password"]}
+19:06:38 fill-VirtualBox sssd_module_for_ansible$ 
+19:06:41 fill-VirtualBox sssd_module_for_ansible$ realm list | grep -i permitted-gr
+  permitted-groups: testgroup1, testgroup2, lalalalalalalagroup
+19:06:50 fill-VirtualBox sssd_module_for_ansible$ 
+19:08:21 fill-VirtualBox sssd_module_for_ansible$ cat example/jsonfile_for_localcheck-delgrp.json 
+{
+    "ANSIBLE_MODULE_ARGS": {
+        "domain_group": "lalalalalalalagroup",
+        "group_state": "absent"
+    }
+}
+19:08:26 fill-VirtualBox sssd_module_for_ansible$ python3 sssd.py example/jsonfile_for_localcheck-delgrp.json
+realm: Specifying deny without --all is deprecated. Use realm permit --withdraw
+
+{"changed": true, "stdout": "Group lalalalalalalagroup successfully removed from the server", "invocation": {"module_args": {"domain_group": "lalalalalalalagroup", "group_state": "absent", "place_default_config": false, "name": null, "in_domain": null, "admin_user": null, "admin_password": null, "computer_ou": null}}, "warnings": ["Module did not set no_log for admin_password"]}
+19:08:42 fill-VirtualBox sssd_module_for_ansible$ realm list | grep -i permitted-gr
+  permitted-groups: testgroup1, testgroup2
+19:08:45 fill-VirtualBox sssd_module_for_ansible$ 
+```
+
+### TODO
+- PlaceDefaultConfigClass not implemented yet
+- JoinOrLeaveDomainClass not implemented yet
+- AddOrDelGroupClass cant handle blank stdout of 'realm list' (when "server is not added to a domain" = "empty /etc/sssd/sssd.conf" ) -> 'realm permit -g somegroup' returns rc=1 and stdout "realm: Couldn't find a configured realm"
+- AddOrDelGroupClass cant work with python2 remote-host interpreter because of usage imported 'shutil' (which can serve with python3 only)
+- AddOrDelGroupClass cant handle multiple groups pass 
+- AddOrDelGroupClass cant handle invalid symbols (when you pass only a comma to domain_group like this `domain_group=,`)
+- AddOrDelGroupClass cant handle exception when you pass empty var domain_group like this  `domain_group=`
+- something else. I forgot ¯\_(ツ)_/¯
--- sssd.py
+++ sssd.py
@@ -1,3 +1,400 @@
 #!/usr/bin/python3
 
 
+
+
+
+
+#KEYWORDHERE! This module works only when played with python3 interpreter at the target host. You MUST set in the  local ansible.cfg an "interpreter = python3" OR set var "ansible_python_interpreter: python3" in the play/role
+
+#"the default" - ansible class import (https://docs.ansible.com/ansible/latest/dev_guide/developing_modules_general.html)
+from __future__ import (absolute_import, division, print_function)
+
+
+#"the default" - ansible class import (https://docs.ansible.com/ansible/latest/dev_guide/developing_modules_general.html)
+#KEYWORDHERE!  This still DOES NOT work WHEN you start this module with `python2 library/sssd.py json_to_local_check-addgrp.json`
+from ansible.module_utils.basic import AnsibleModule
+#The error looks llike that:
+# username@SERVERNAME myfolder $ python2 library/sssd.py json_to_local_check-addgrp.json
+# Traceback (most recent call last):
+#   File "library/sssd.py", line 404, in <module>
+#     main()
+#   File "library/sssd.py", line 251, in main
+#      from ansible.module_utils.basic import AnsibleModule
+# ImportError: No module named ansible.module_utils.basic
+# username@SERVERNAME myfolder $
+
+
+import subprocess
+import os
+import shutil
+
+#"the default" - ansible class import (https://docs.ansible.com/ansible/latest/dev_guide/developing_modules_general.html)
+__metaclass__ = type
+
+#"the default" - ansible class import (https://docs.ansible.com/ansible/latest/dev_guide/developing_modules_general.html)
+DOCUMENTATION = r'''
+---
+module: sssd
+
+short_description: This is my module to manage sssd
+
+version_added: "1.0.0"
+
+description: This is my module to manage sssd. ONLY PYTHON3 REMOTE HOST INTERPRETER IS AVAILABLE! works ONLY WHEN you set in the  local ansible.cfg an "interpreter = python3" OR set var "ansible_python_interpreter: python3" in the play/role
+
+author:
+    - Alexander Leontev
+    - somebody_else
+'''
+
+EXAMPLES = r'''
+# Pass in a message
+- name: add a single domain group
+  sssd:
+    domain_group: lalalagroup
+    group_state: present
+# Add a single group - same as a: `realm permit -g domain_group`
+
+- name: add a single domain group
+  sssd:
+    domain_group: lalalagroup
+    group_state: absent
+# remove a single group - same as a: `realm deny -g domain_group`
+
+- name: add a multiple domain group (NOT IMPLEMENTED YET)
+  sssd:
+    domain_group: lalalagroup pupupugroup
+    group_state: present
+# Add a multiple domain groups - same as a: `realm permit -g domain_group1 domain_group2`
+
+- name: add a multiple domain group (NOT IMPLEMENTED YET)
+  sssd:
+    domain_group: lalalagroup pupupugroup
+    group_state: absent
+# remove a multiple groups  - same as a: `realm deny -g domain_group1 domain_group2`
+
+- name: create default sssd config 
+  sssd:
+    place_default_config: True
+# create default sssd config (NOT IMPLEMENTED YET. I do not know what command to use)
+
+- name: join server to an active directory domain (NOT IMPLEMENTED YET)
+  sssd:
+    in_domain: present
+    domain_name: example.com
+    admin_user: 
+    admin_password: 
+    computer_ou: 
+# join server to an active directory domain `echo '{{ admin_password }}' | realm join --computer-ou='{{ sssd_ou }}' --verbose example.com -U '{{ admin_user }}'`
+
+
+- name: remove server from an active directory domain  (NOT IMPLEMENTED YET)
+  sssd:
+    in_domain: absent
+    admin_user: 
+    admin_password: 
+#
+# remove server from an active directory domain (will delete an /etc/sssd/sssd.conf and keytab too!!!) - same as a: `realm leave -U {{ admin_user }}`
+'''
+
+#Let's describe our CLASSes
+class AddOrDelGroupClass(object):
+    def __init__(self, module):
+        global errorvar
+        errorvar = None
+        try:
+            #check if 'realm list' command tell  us NOT any exception
+            checkvar = subprocess.Popen(['realm','list'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            streamdata = checkvar.communicate()[0]
+            rc = checkvar.returncode
+            #print(rc)
+        except Exception:
+            #if any exception occurs (ex. binary not found in the PATH), we will set errorvar = 1
+            #print("EXCEPTION!")
+            errorvar = 1
+            #print(errorvar)
+        self.module = module
+        self.domain_group = module.params['domain_group']
+        self.group_state = module.params['group_state']
+        self.isgroupalreadyexists = self.IsGroupAlreadyExistsFunc()
+        #print(self.isgroupalreadyexists)
+
+    def IsGroupAlreadyExistsFunc(self):
+        #Here we detect if group is added to realm on the server or not
+        # KEYWORDHERE! This works when "interpreter_python = python3" in local ansible.cfg
+        # KEYWORDHERE! This DOES NOT work when "interpreter_python = auto_silent" in local ansible.cfg
+        utilpath = shutil.which("realm")
+        if utilpath:
+            checkargstring = 'realm list | grep permitted-groups'
+            listofgroups = subprocess.check_output(checkargstring, shell=True).decode('utf-8').strip().split('\n')
+            return listofgroups
+        else:
+            return "no gr"
+        #return
+
+    def ErrorCallFunc1(self):
+        #return format have to be "rc, out, err, msg, failed"
+        return 2, "", "ERR_ group_state IS NOT defined AS absent or present", "MSG_ group_state IS NOT defined AS absent or present", "fall to failed state"
+
+    def ErrorCallFunc2(self):
+        # return format have to be "rc, out, err, msg, failed"
+        messages = "during manipulation of domain groups your args group_state and domain_group SHOULD BE DEFINED, and other args SHOULD NOT BE DEFINED"
+        return 2, "", "ERR_ %(insertion)s" % {"insertion": messages}, "MSG_ %(insertion)s" % {"insertion": messages}, "fall to failed state"
+      
+    def AddGroupFunc(self):
+      ###the main logic of function is here
+      # return format have to be "rc, out, err, msg, failed"
+        if errorvar == 1:
+            return 2, "", "ERR_ cant find binary realm. sssd SHOULD be installed! you SHOULD use become!", "MSG_", "fall to failed state"
+        else:
+            #debug print
+            #print(errorvar)
+            try:
+                ###NOT DONE YET! Here should  create a new logic to determine if there are more than one word in self.domain_group (if user trying to pass multiple groups). Now we fall to 'if' in this way BUT WE SHOULD NOT
+                #this tell us if group exists
+                if any(self.domain_group in item for item in self.isgroupalreadyexists):
+                    return 2, "Group %(insertion)s has ALREADY been added to the server" % {"insertion": self.domain_group}, "", "", ""
+                else:
+                    #this will add a group (we fall here because group is not present)
+                    tmpargstring = 'realm permit -g ' + self.domain_group
+                    subprocess.check_call(tmpargstring, shell=True)
+                    return 0, "Group %(insertion)s now added to the server" % {"insertion": self.domain_group}, "", "", ""
+            except subprocess.CalledProcessError:
+                return 2, "Group %(insertion)s NOT added to the server" % {"insertion": self.domain_group}, "Something went wrong", "", 1
+
+        #return lalala
+
+    def RemoveGroupFunc(self):
+        ###The main logic of function is here
+        # return format have to be "rc, out, err, msg, failed"
+        if errorvar == 1:
+            return 2, "", "ERR_ cant find binary realm. sssd SHOULD be installed! you SHOULD use become!", "MSG_", "fall to failed state"
+        else:
+            #debug print
+            #print(errorvar)
+            try:
+                ###NOT DONE YET! Here should  create a new logic to determine if there are more than one word in self.domain_group (if user trying to pass multiple groups). Now we fall to 'else' in ths way BUT WE SHOULD NOT
+                #this tell us if group exists
+                if any(self.domain_group in item for item in self.isgroupalreadyexists):
+                    tmpargstring = 'realm deny -g ' + self.domain_group
+                    subprocess.check_call(tmpargstring, shell=True)
+                    return 0, "Group %(insertion)s successfully removed from the server" % {"insertion": self.domain_group}, "", "", ""
+                else:
+                    return 2, "Group %(insertion)s not present on the server" % {"insertion": self.domain_group}, "", "", ""
+            except Exception:# as e
+                return 2, "undefined error while deleting group from the server" % {"insertion": self.domain_group}, "Something went wrong", "", 1
+    #            if e :
+    #                return 2, "undefined error while deleting group from the server" % {"insertion": self.domain_group}, "Something went wrong", "", 1
+    #            elif e :
+    #                return 2, "undefined error while deleting group from the server" % {"insertion": self.domain_group}, "Something went wrong", "", 1
+    #            else
+    #                return 2, "undefined error while deleting group from the server" % {"insertion": self.domain_group}, "Something went wrong", "", 1
+
+
+
+
+class PlaceDefaultConfigClass(object):
+    def __init__(self, module):
+        self.module = module
+        self.place_default_config = module.params['place_default_config']
+
+    def PlaceConfigFunc(selfself):
+        ###The main logic of function is here
+        # return format have to be "rc, out, err, msg, failed"
+        ###here should be a logic to
+        ###1) backup existent config
+        ###2) place default config
+        ###3) restart sssd.service if needed
+        print("existent config backuped and new default config placed")
+        #return lalala
+
+    def ErrorCallFunc_1(self):
+        # return format have to be "rc, out, err, msg, failed"
+        messages = "during manipulation of sssd config file your arg place_default_config SHOULD BE DEFINED, and other args SHOULD NOT BE DEFINED"
+        return 2, "", "ERR_ %(insertion)s" % {"insertion": messages}, "MSG_ %(insertion)s" % {"insertion": messages}, "fall to failed state"
+
+
+
+class JoinOrLeaveDomainClass(object):
+    def __init__(self, module):
+        self.module = module
+        self.in_domain = module.params['in_domain']
+        self.admin_user = module.params['admin_user']
+        self.admin_password = module.params['admin_password']
+        self.computer_ou = module.params['computer_ou']
+
+    def JoinDomainFunc(self):
+        ###The main logic of function is here
+        # return format have to be "rc, out, err, msg, failed"
+        ###here should be a logic to
+        ###1) check if we are already in the domain
+        ###2) check if domain controller (we can get it from Host_var/Group_var or sssd config) is available
+        ###3) join domain if not joined
+        print("Joined to domain")
+        #return lalala
+
+    def LeaveDomainFunc(self):
+        ###The main logic of function is here
+        # return format have to be "rc, out, err, msg, failed"
+        ###here should be a logic to
+        ###1) check if we are already in the domain
+        ###2) leave domain if  joined
+        print("Removed from domain")
+        #return lalala
+
+
+
+def main():
+
+    module_args = {
+        "name": {"type": "str", "required": False},
+        "domain_group": {"type": "str", "required": False},
+        "group_state": {"type": "str", "required": False},
+        "place_default_config": {"type": "bool", "required": False, "default": "False"},
+        "in_domain": {"type": "str", "required": False},
+        "admin_user": {"type": "str", "required": False},
+        "admin_password": {"type": "str", "required": False},
+        "computer_ou": {"type": "str", "required": False},
+    }
+
+    #"the default" - ansible class import (https://docs.ansible.com/ansible/latest/dev_guide/developing_modules_general.html)
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=False
+    )
+
+    #"the default" - ansible class import (https://docs.ansible.com/ansible/latest/dev_guide/developing_modules_general.html)
+    #We will not use it ()
+    #if module.check_mode:
+    #    module.exit_json(**result)
+
+
+    rc = None
+    out = ''
+    err = ''
+    msg = None
+    failed = None
+
+    result = {}
+
+    #Get args passed to the module by user/play/role
+    flag_group_state = module.params['group_state']
+    flag_domain_group = module.params['domain_group']
+    flag_place_default_config = module.params['place_default_config']
+    flag_in_domain = module.params['in_domain']
+    flag_admin_user = module.params['admin_user']
+    flag_admin_password = module.params['admin_password']
+    flag_computer_ou = module.params['computer_ou']
+
+
+
+    #initialize our own CLASSes
+    #The "__init__" function of each class will start automatically when we initialize each class
+    GroupClassCall = AddOrDelGroupClass(module)
+    DefaultconfigClassCall = PlaceDefaultConfigClass(module)
+    DomainClassCall = JoinOrLeaveDomainClass(module)
+
+
+
+
+
+    ###THE MAIN LOGIC BLOCK BEGINS HERE###
+
+    ###we can ONLY use flag_in_domain and flag_admin_user and flag_admin_password and flag_computer_ou separately from all other flags
+    ###if it is - let our logic run
+    if (
+        flag_domain_group is None and
+        flag_group_state is None and
+        flag_place_default_config is False and
+        flag_in_domain is not None and
+        flag_admin_user is not None and
+        flag_admin_password is not None and
+        flag_computer_ou is not None):
+        if module.params['in_domain'] == 'present':
+            print("try to call JoinDomainFunc here")
+            rc, out, err, msg, failed = DomainClassCall.JoinDomainFunc()
+        elif module.params['in_domain'] == 'absent':
+            print("try to call LeaveDomainFunc here")
+            rc, out, err, msg, failed = DomainClassCall.LeaveDomainFunc()
+        else:
+            print('in_domain IS NOT defined AS absent or present')
+        #else:
+            #print('during manipulation of the state of presence in a domain group your args "in_domain" and "admin_user" and "admin_password" and "computer_ou" SHOULD BE DEFINED, and other args SHOULD NOT BE DEFINED ')
+
+
+    ###we can ONLY use flag_place_default_config separately from all other flags
+    ###if it is - let our logic run
+    if (
+        flag_domain_group is None and
+        flag_group_state is None and
+        flag_place_default_config is not False and
+        flag_in_domain is None and
+        flag_admin_user is None and
+        flag_admin_password is None and
+        flag_computer_ou is None):
+        if module.params['place_default_config'] == 'True':
+            print("try to call PlaceConfigFunc here")
+            rc, out, err, msg, failed = DefaultconfigClassCall.PlaceConfigFunc()
+        elif module.params['place_default_config'] == 'False':
+            print("You can use only place_default_config=True")
+        else:
+            print("place_default_config IS NOT defined AS True or False")
+    #else:
+        #print('#during manipulation of sssd config file your arg "place_default_config" should BE DEFINED, and other args SHOULD NOT BE DEFINED')
+        #print(flag_place_default_config)
+        #rc, out, err, msg, failed = DefaultconfigClassCall.ErrorCallFunc_1()
+
+
+    ###we can ONLY use flag_domain_group and flag_group_state separately from all other flags
+    ###if it is - let our logic run
+    if (
+        flag_domain_group is not None and
+        flag_group_state is not None and
+        flag_place_default_config is False and
+        flag_in_domain is None and
+        flag_admin_user is None and
+        flag_admin_password is None and
+        flag_computer_ou is None):
+        if module.params['group_state'] == 'present':
+            #print("try to call AddGroupFunc here")
+            rc, out, err, msg, failed = GroupClassCall.AddGroupFunc()
+        elif module.params['group_state'] == 'absent':
+            #print("try to call RemoveGroupFunc here")
+            rc, out, err, msg, failed = GroupClassCall.RemoveGroupFunc()
+        else:
+            #print('group_state IS NOT defined AS absent or present')
+            rc, out, err, msg, failed = GroupClassCall.ErrorCallFunc1()
+    #else:
+        #print('#during manipulation of domain groups your arg "group_state" and "domain_group" SHOULD  BE DEFINED, and other args SHOULD NOT BE DEFINED')
+        #rc, out, err, msg, failed = GroupClassCall.ErrorCallFunc2()
+
+    # Here are standard things "rc, out, err, msg, failed" described. Ansible uses some of this things to determine if we are FAILD or SUCESS or CHANGED.
+    # ansible framework status FAILED  needs simultaneously: "result['err']" defined as string and "result['msg']" defined as string and "result['changed'] = False" and "result['failed'] = True"
+    # ansible framework status CHANGED needs simultaneously: "result['changed'] = True" and  "result['stdout']" defined as string and "result['stderr'] = None"
+    # ansible framework status SUCESS  needs simultaneously: "result['changed'] = False" and "result['stdout']" defined as string
+    if rc is None:
+        result['changed'] = False
+    elif rc == 2:
+        result['changed'] = False
+    else:
+        result['changed'] = True
+
+    if out:
+        result['stdout'] = out
+
+    if err:
+        result['stderr'] = err
+
+    if msg:
+        result['msg'] = msg
+
+    if failed:
+        result['failed'] = True
+
+    ###THE MAIN LOGIC BLOCK ENDS HERE###
+    module.exit_json(**result)
+
+
+if __name__ == '__main__':
+    main()
